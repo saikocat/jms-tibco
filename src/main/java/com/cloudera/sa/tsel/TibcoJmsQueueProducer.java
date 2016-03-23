cbff2b1052f728ff986a61be85edd158ef7a15a6
Hoa Duc Nguyen <hoa@cloudera.com>
2016-03-23 16:40:08 +0700

    Add Tibco Jms Queue Producer (Sync + Transactional)

---
+++ src/main/java/com/cloudera/sa/tsel/TibcoJmsQueueProducer.java
@@ -0,0 +1,100 @@
+package com.cloudera.sa.tsel;
+
+import com.tibco.tibjms.TibjmsConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import javax.jms.JMSException;
+
+public class TibcoJmsQueueProducer {
+    private static final boolean AUTOCOMMIT = false;
+    private static final boolean TRANSACTED = true;
+
+    private final ConnectionFactory factory;
+    private final Connection connection;
+    private final Session session;
+    private final MessageProducer producer;
+    private final Destination destination;
+
+    public TibcoJmsQueueProducer(
+            final ConnectionFactory factory, final Connection connection,
+            final Session session, final Destination destination,
+            final MessageProducer producer) throws JMSException {
+        this.factory = factory;
+        this.connection = connection;
+        this.session = session;
+        this.destination = destination;
+        this.producer = producer;
+    }
+
+    public void start() throws JMSException {
+        connection.start();
+    }
+
+    public void commit() throws JMSException {
+        session.commit();
+    }
+
+    public void rollback() throws JMSException {
+        session.rollback();
+    }
+
+    public void close() throws JMSException {
+        session.close();
+        connection.close();
+    }
+
+    public void queue(final String event, boolean autocommit) throws JMSException {
+        TextMessage message = session.createTextMessage();
+        message.setText(event);
+        producer.send(message);
+        if (autocommit)
+            session.commit();
+    }
+
+    public void queue(final String event) throws JMSException {
+        queue(event, AUTOCOMMIT);
+    }
+
+    public static class Builder {
+        private ConnectionFactory factory;
+        private Connection connection;
+        private Session session;
+        private MessageProducer producer;
+        private Destination destination;
+
+        public Builder(
+                final String serverUrl,
+                final String userName, final String password) throws JMSException {
+            this.factory = new com.tibco.tibjms.TibjmsConnectionFactory(serverUrl);
+            this.connection = factory.createConnection(userName, password);
+            this.session = connection.createSession(TRANSACTED, Session.SESSION_TRANSACTED);
+        }
+
+        public Builder withDestination(final String destination) throws JMSException {
+            this.destination = this.session.createQueue(destination);
+            return this;
+        }
+
+        public Builder withProducer(final Destination destination) throws JMSException {
+            this.producer = this.session.createProducer(destination);
+            return this;
+        }
+
+        public Builder withProducer() throws JMSException {
+            this.producer = this.session.createProducer(this.destination);
+            return this;
+        }
+
+        public TibcoJmsQueueProducer build() throws JMSException {
+            return new TibcoJmsQueueProducer(
+                this.factory, this.connection, this.session, this.destination,
+                this.producer);
+        }
+    }
+}
