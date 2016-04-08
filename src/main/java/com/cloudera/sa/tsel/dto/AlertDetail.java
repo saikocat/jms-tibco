2ec31a5cc2084a32ff4ab2cf6dd1e598b8ec6212
Hoa Duc Nguyen <hoa@cloudera.com>
2016-04-08 15:07:28 +0800

    Add AlertDetail dto (TIBCO-14)

---
+++ src/main/java/com/cloudera/sa/tsel/dto/AlertDetail.java
@@ -0,0 +1,82 @@
+package com.cloudera.sa.tsel.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+
+import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "Alert_Detail")
+public class AlertDetail {
+
+    private AlertDetail() { }
+
+    public AlertDetail(
+            final String alertId,
+            final String kpiNumber,
+            final String msisdn) {
+        this.alertId = alertId;
+        this.kpiNumber = kpiNumber;
+        this.msisdn = msisdn;
+    }
+
+    @JacksonXmlProperty(localName = "Alert_ID")
+    public String alertId;
+
+    @JacksonXmlProperty(localName = "KPI_Number")
+    public String kpiNumber;
+
+    @JacksonXmlProperty(localName = "MSISDN")
+    public String msisdn;
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("Alert_ID", this.alertId)
+            .add("KPI_Number", this.kpiNumber)
+            .add("MSISDN", this.msisdn)
+            .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        final AlertDetail other = (AlertDetail) obj;
+        return Objects.equal(this.alertId, other.alertId)
+            && Objects.equal(this.kpiNumber, other.kpiNumber)
+            && Objects.equal(this.msisdn, other.msisdn);
+    }
+
+    public static class Builder {
+        private String alertId;
+        private String kpiNumber;
+        private String msisdn;
+
+        public Builder() {
+        }
+
+        public Builder withAlertId(final String alertId) {
+            this.alertId = alertId;
+            return this;
+        }
+
+        public Builder withKpiNumber(final String kpiNumber) {
+            this.kpiNumber = kpiNumber;
+            return this;
+        }
+
+        public Builder withMsisdn(final String msisdn) {
+            this.msisdn = msisdn;
+            return this;
+        }
+
+        public AlertDetail build() {
+            return new AlertDetail(this.alertId, this.kpiNumber, this.msisdn);
+        }
+    }
+}
---
+++ src/main/java/com/cloudera/sa/tsel/dto/AlertDetails.java
@@ -0,0 +1,58 @@
+package com.cloudera.sa.tsel.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+
+import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "Alert_Details")
+public class AlertDetails {
+    @JacksonXmlProperty(localName = "Alert_Detail")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    public AlertDetail[] alertDetails = new AlertDetail[0];
+
+    private AlertDetails() {}
+
+    public AlertDetails(final AlertDetail[] alertDetails) {
+        this.alertDetails = alertDetails;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("KPI_Detail", this.alertDetails)
+            .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        final AlertDetails other = (AlertDetails) obj;
+        return java.util.Arrays.deepEquals(this.alertDetails, other.alertDetails);
+    }
+
+    public static class Builder {
+        private AlertDetail[] alertDetails;
+
+        public Builder() {
+        }
+
+        public Builder withAlertDetails(List<AlertDetail> alertDetails) {
+            this.alertDetails = new ImmutableList.Builder<AlertDetail>()
+                .addAll(alertDetails)
+                .build()
+                .toArray(new AlertDetail[0]);
+            return this;
+        }
+
+        public AlertDetails build() {
+            return new AlertDetails(this.alertDetails);
+        }
+    }
+}
