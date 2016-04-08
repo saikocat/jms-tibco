51af7275bae1b7fd04f85ca4bd8aad8123010f1a
Hoa Duc Nguyen <hoa@cloudera.com>
2016-04-08 17:56:46 +0800

    Add serdes test for Alert and Ticket Details (TIBCO-14)

--- src/test/java/com/cloudera/sa/tsel/TibcoRuleMessageSerDesTest.java
+++ src/test/java/com/cloudera/sa/tsel/TibcoRuleMessageSerDesTest.java
@@ -37,6 +37,10 @@ public class TibcoRuleMessageSerDesTest {
             .enable(SerializationFeature.INDENT_OUTPUT);
     }
 
+    // WARNING: Be careful about testing string comparision for xml here
+    // This is rather lazy way of testing so the content must match exactly
+    // Common thing to look out in xml is the use of ' and ", UTF-8 capitalization
+
     @Test
     public void shouldDeserialize_KpiDetails() throws URISyntaxException,
            IOException, UnsupportedEncodingException {
@@ -51,6 +55,38 @@ public class TibcoRuleMessageSerDesTest {
         KpiDetails kds = createKpiDetails();
         assertEquals(TibcoRuleMessageSerDes.serialize(kds),
                      getFileContent("/kpi-details.xml"));
+    }
+
+    @Test
+    public void shouldDeserialize_AlertDetails() throws URISyntaxException,
+           IOException, UnsupportedEncodingException {
+        String xml = getFileContent("/alert-details.xml");
+        assertEquals(TibcoRuleMessageSerDes.deserialize(xml, AlertDetails.class),
+                     createAlertDetails());
+    }
+
+    @Test
+    public void shouldSerialize_AlertDetails() throws URISyntaxException,
+           IOException, UnsupportedEncodingException, JsonProcessingException {
+        AlertDetails kds = createAlertDetails();
+        assertEquals(TibcoRuleMessageSerDes.serialize(kds),
+                     getFileContent("/alert-details.xml"));
+    }
+
+    @Test
+    public void shouldDeserialize_TicketDetails() throws URISyntaxException,
+           IOException, UnsupportedEncodingException {
+        String xml = getFileContent("/ticket-details.xml");
+        assertEquals(TibcoRuleMessageSerDes.deserialize(xml, TicketDetails.class),
+                     createTicketDetails());
+    }
+
+    @Test
+    public void shouldSerialize_TicketDetails() throws URISyntaxException,
+           IOException, UnsupportedEncodingException, JsonProcessingException {
+        TicketDetails kds = createTicketDetails();
+        assertEquals(TibcoRuleMessageSerDes.serialize(kds),
+                     getFileContent("/ticket-details.xml"));
     }
 
     public String getFileContent(String resourceUrl) throws URISyntaxException,
@@ -83,4 +119,31 @@ public class TibcoRuleMessageSerDesTest {
         return kds;
     }
 
+    public AlertDetails createAlertDetails() {
+        AlertDetail ad = new AlertDetail.Builder()
+            .withAlertId("alert_id:str")
+            .withKpiNumber("kpi_number:str")
+            .withMsisdn("msisdn:str")
+            .build();
+
+        AlertDetails ads = new AlertDetails.Builder()
+            .withAlertDetails(ImmutableList.<AlertDetail>of(ad).asList())
+            .build();
+
+        return ads;
+    }
+
+    public TicketDetails createTicketDetails() {
+        TicketDetail td = new TicketDetail.Builder()
+            .withAlertId("alert_id:str")
+            .withTicketId("ticket_id:str")
+            .withMsisdn("msisdn:str")
+            .build();
+
+        TicketDetails tds = new TicketDetails.Builder()
+            .withTicketDetails(ImmutableList.<TicketDetail>of(td).asList())
+            .build();
+
+        return tds;
+    }
 }
