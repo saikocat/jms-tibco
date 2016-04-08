cb2f17b9664f8c56894ff66fbfe0a19fc0352954
Hoa Duc Nguyen <hoa@cloudera.com>
2016-04-08 15:07:52 +0800

    Add TicketDetail dto (TIBCO-14)

---
+++ src/main/java/com/cloudera/sa/tsel/dto/TicketDetail.java
@@ -0,0 +1,82 @@
+package com.cloudera.sa.tsel.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+
+import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "Ticket_Detail")
+public class TicketDetail {
+
+    private TicketDetail() { }
+
+    public TicketDetail(
+            final String alertId,
+            final String ticketId,
+            final String msisdn) {
+        this.alertId = alertId;
+        this.ticketId = ticketId;
+        this.msisdn = msisdn;
+    }
+
+    @JacksonXmlProperty(localName = "Alert_ID")
+    public String alertId;
+
+    @JacksonXmlProperty(localName = "Ticket_ID")
+    public String ticketId;
+
+    @JacksonXmlProperty(localName = "MSISDN")
+    public String msisdn;
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("Alert_ID", this.alertId)
+            .add("Ticket_ID", this.ticketId)
+            .add("MSISDN", this.msisdn)
+            .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        final TicketDetail other = (TicketDetail) obj;
+        return Objects.equal(this.alertId, other.alertId)
+            && Objects.equal(this.ticketId, other.ticketId)
+            && Objects.equal(this.msisdn, other.msisdn);
+    }
+
+    public static class Builder {
+        private String alertId;
+        private String kpiNumber;
+        private String msisdn;
+
+        public Builder() {
+        }
+
+        public Builder withTicketId(final String alertId) {
+            this.alertId = alertId;
+            return this;
+        }
+
+        public Builder withKpiNumber(final String kpiNumber) {
+            this.kpiNumber = kpiNumber;
+            return this;
+        }
+
+        public Builder withMsisdn(final String msisdn) {
+            this.msisdn = msisdn;
+            return this;
+        }
+
+        public TicketDetail build() {
+            return new TicketDetail(this.alertId, this.kpiNumber, this.msisdn);
+        }
+    }
+}
---
+++ src/main/java/com/cloudera/sa/tsel/dto/TicketDetails.java
@@ -0,0 +1,58 @@
+package com.cloudera.sa.tsel.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+
+import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "Ticket_Details")
+public class TicketDetails {
+    @JacksonXmlProperty(localName = "Ticket_Detail")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    public TicketDetail[] ticketDetails = new TicketDetail[0];
+
+    private TicketDetails() {}
+
+    public TicketDetails(final TicketDetail[] ticketDetails) {
+        this.ticketDetails = ticketDetails;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("Ticket_Details", this.ticketDetails)
+            .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        final TicketDetails other = (TicketDetails) obj;
+        return java.util.Arrays.deepEquals(this.ticketDetails, other.ticketDetails);
+    }
+
+    public static class Builder {
+        private TicketDetail[] ticketDetails;
+
+        public Builder() {
+        }
+
+        public Builder withTicketDetails(List<TicketDetail> ticketDetails) {
+            this.ticketDetails = new ImmutableList.Builder<TicketDetail>()
+                .addAll(ticketDetails)
+                .build()
+                .toArray(new TicketDetail[0]);
+            return this;
+        }
+
+        public TicketDetails build() {
+            return new TicketDetails(this.ticketDetails);
+        }
+    }
+}
